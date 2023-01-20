418eb083673f6b5d3efee4f85525e9da84dd7230
BAEJUN SON <sonqowns2@naver.com>
2023-01-20 22:30:53 +0900

    Feat : 카카오맵 기능 추가 Date : 2023 / 01 / 20 Branch : master State : 미완 Comment :  - 검색창UI 수정  - 데이터 렌더링 기능 추가

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -1,28 +1,44 @@
 <template>
   <div>
-    <div>
-      <button @click="savePlan">플랜저장하기</button>
-    </div>
-    <div class="searchbox">
-      <div>
-        <input type="text" value="창원대학교" @keyup.enter="searchPlaces" />
-      </div>
-      <div class="results">
-        <div class="place" v-for="rs in search.results" :key="rs.id">
-          <h4 @click="setCenter(rs)" style="cursor: pointer;">{{ rs.place_name }}</h4>
-          <h4>{{ rs.x }}</h4>
-          <h4>{{ rs.y }}</h4>
-          <input type="text" placeholder="메모를 입력하세요" />
-          <button @click="addPlan(rs)">+</button>
+    <button @click="zoom(1)">+</button> <button @click="zoom(-1)">-</button>
+    <div class="maparea">
+      <div class="searchbox">
+        <div>
+          <input
+            type="text"
+            placeholder="장소를 검색해주세요."
+            @keyup.enter="searchPlaces"
+          />
+        </div>
+        <div class="results">
+          <div class="place" v-for="rs in search.results" :key="rs.id">
+            <h4 @click="setCenter(rs)" style="cursor: pointer">
+              {{ rs.place_name }}
+            </h4>
+            <div class="addr">
+              <h5>{{ rs.address_name }}</h5>
+            </div>
+            <button @click="addPlan(rs)">+</button>
+          </div>
         </div>
       </div>
+      <div id="map"></div>
     </div>
-    <button @click="zoom(1)">+</button> <button @click="zoom(-1)">-</button>
-    <div id="map"></div>
+    <div class="planner">
+      <button @click="savePlan">플랜저장하기</button>
+      <button @click="getPlan">플랜갖고오기</button>
+      <div class="plan" v-for="rs in planner.plan" :key="rs.name">
+        <h4>{{ rs.name }}</h4>
+        <input type="text" v-model="rs.memo" placeholder="메모를 입력하세요." />
+        <input type="text" v-model="rs.date" placeholder="YYYY-MM-DD HH:MM" />
+      </div>
+    </div>
   </div>
 </template>
   
 <script>
+import axios from "axios";
+
 export default {
   name: "KakaoMap",
   data() {
@@ -39,11 +55,19 @@ export default {
         pgn: null,
         results: [],
       },
+      planner: {
+        title: "test",
+        intro: "intro",
+        start_date: "2022-01-02T13:00+09:00",
+        end_date: "2022-02-02T01:00+09:00",
+        plan: [],
+      },
       map: null,
       plan: [],
       plan2: [],
     };
   },
+  watch: {},
   mounted() {
     if (window.kakao && window.kakao.maps) {
       // 카카오 객체가 있고, 카카오 맵 그릴 준비가 되어 있다면 맵 실행
@@ -90,7 +114,6 @@ export default {
 
       const ps = new window.kakao.maps.services.Places();
       ps.keywordSearch(keyword, (data, status, pgn) => {
-        console.log(data.lat, data.lng);
         this.search.keyword = keyword;
         this.search.pgn = pgn;
         this.search.results = data;
@@ -98,15 +121,36 @@ export default {
     },
     addPlan(rs) {
       let obj = {};
-      obj.placeName = rs.place_name;
+      obj.name = rs.place_name;
+      obj.memo = "";
+      obj.date = "2023-01-01 01:30";
       obj.x = rs.x;
       obj.y = rs.y;
-      console.log(this.plan);
-      this.plan.push(obj);
+      this.planner.plan.push(obj);
     },
     savePlan() {
-      this.plan2.push(this.plan);
-      console.log(this.plan2);
+      /* axios
+        .post("https://42b1923e-9ac4-4979-b904-912c15c18ea6.mock.pstmn.io/localhost:8080/planner/{id}", this.planner)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        }); */
+    },
+    getPlan() {
+      axios
+        .get(
+          "https://42b1923e-9ac4-4979-b904-912c15c18ea6.mock.pstmn.io/localhost:8080/api/v1/planner"
+        )
+        .then((response) => {
+          response.data.plan.forEach((a) => {
+            this.planner.plan.push(a);
+          });
+        })
+        .catch(() => {
+          console.log("실패");
+        });
     },
     setCenter(rs) {
       // 이동할 위도 경도 위치를 생성합니다
@@ -122,15 +166,56 @@ export default {
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped>
 #map {
-  width: 100%;
-  height: 400px;
+  flex: 1 1 auto;
+  height: 500px;
 }
-.place {
-  border: 1px solid red;
+.maparea {
+  width: 80%;
+  display: flex;
+  position: relative;
+}
+.maparea h4 {
+  margin: 0;
+}
+.maparea h5 {
+  margin: 0;
+}
+.searchbox {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 500px;
+  z-index: 10000;
+  background-color: #ffffffaa;
+  overflow-y: auto;
+  width: 250px;
+  display: flex;
+  flex-direction: column;
 }
 
-.result {
-  overflow: scroll;
+.place {
+  padding: 8px;
+}
+.plan {
+  top: 0;
+  left: 0;
+  background-color: #ffffffaa;
+  overflow-y: auto;
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  margin: 5px;
+}
+
+.plan h4 {
+  margin: 0;
+}
+.plan input {
+  margin: 3px;
+}
+
+.planner button {
+  margin: 3px;
 }
 </style>
   
