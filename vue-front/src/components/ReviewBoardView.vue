e57755a97a8dbea4639df63ffd3e9f8fcf89dfcd
BAEJUN SON <sonqowns2@naver.com>
2023-03-21 20:31:14 +0900

    feat : 게시글 상세조회 컴포넌트 추가 및 작성 컴포넌트 기능 추가 date : 2023 / 03 / 21 branch : master state : 미완 comment :  - 게시글 상세조회 컴포넌트 추가  - 게시글 수정 시 작성창에서 수정 처리하는 기능 추가

---
+++ vue-front/src/components/ReviewBoardView.vue
@@ -0,0 +1,308 @@
+<template>
+  <div>
+    <v-card style="width: 90%; margin-left: 35px" elevation="6">
+      <div class="view_header1">
+        <div>{{ board.title }}</div>
+        <div class="manageBtn" v-if="checkBoardUser == 0">
+          <button class="editBtn" @click="goEdit()">수정</button>
+          <button class="deleteBtn" @click="deleteBoard()">삭제</button>
+        </div>
+      </div>
+      <div class="view_header2">
+        <div id="viewUser"><i class="fas fa-user"></i>{{ board.nickname }}</div>
+        <div id="viewDate"><i class="fas fa-clock"></i> {{ viewDate }}</div>
+      </div>
+      <div class="viewContent" v-text="board.content"></div>
+    </v-card>
+    <div class="balloon">
+      <div id="comment_num">{{ board.commentResponseDtoList.length }}</div>
+      <div id="comment_title">Comments</div>
+    </div>
+    <div v-for="item in board.commentResponseDtoList" :key="item.comment">
+      <v-card style="width: 90%; margin-left: 35px" elevation="4">
+        <div class="comment_header">
+          <div id="viewUser">
+            <i class="fas fa-user"></i>{{ item.nickname }}
+            <div class="manageComment" v-if="item.nickname == curNickname">
+              <button><i class="fas fa-edit"></i></button>
+              <button><i class="fas fa-trash"></i></button>
+            </div>
+          </div>
+          <div id="viewDate">
+            <i class="fas fa-clock"></i> {{ item.createdDate }}
+          </div>
+        </div>
+        <div class="commentContent" v-text="item.comment"></div>
+      </v-card>
+    </div>
+    <div>
+      <v-textarea
+        outlined
+        name="input-7-4"
+        style="width: 90%; margin: 20px 0px 0px 35px"
+        label="댓글을 남겨보세요"
+        auto-grow
+        rows="4"
+        row-height="30"
+        shaped
+        v-model="comment.comment"
+      ></v-textarea>
+      <button class="writeComment" @click="writeComment">댓글작성</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      boardNickname: "",
+      curNickname: "",
+      viewDate: "",
+      checkBoardUser: 0,
+      comment: {
+        comment: "",
+      },
+      board: {
+        title: "",
+        nickname: "",
+        content: "",
+        createdDate: "",
+        modifiedDate: "",
+        commentResponseDtoList: [],
+      },
+    };
+  },
+  mounted() {
+    this.curNickname = this.$store.state.userInfo.nickname;
+    console.log(this.$route.params.id);
+    this.id = this.$route.params.id;
+    this.getBoard();
+    this.id = this.$route.params.id;
+    console.log(this.board);
+  },
+  methods: {
+    writeComment() {
+      //https://reqres.in/api/users
+      //http://localhost:8080/api/v1/board/comments/post/${this.id}
+      axios({
+        method: "post", // [요청 타입]
+        url: `https://reqres.in/api/users`, // [요청 주소]
+        data: JSON.stringify(this.comment), // [요청 데이터]
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "X-AUTH-TOKEN": localStorage.getItem("access_token"),
+        }, // [요청 헤더]
+        timeout: 5000, // [타임 아웃 시간]
+
+        //responseType: "json" // [응답 데이터 : stream , json]
+      })
+        .then((response) => {
+          console.log(response.data);
+          this.board.commentResponseDtoList.length = 0;
+          this.getBoard();
+        })
+        .catch((error) => {
+          //비로그인 시 로그인 창으로 이동
+          if (error.stateCode == "403") {
+            alert("로그인이 필요한 서비스입니다.");
+            this.$router.push({ name: "Login" });
+            return;
+          }
+          //게시물을 찾을 수 없을 시 게시판 메인화면으로 이동
+          else if (error.stateCode == "404") {
+            alert("게시물을 찾을 수 없습니다.");
+            this.$router.push({ name: "ReviewBoard" });
+          }
+          console.log("ERROR : " + JSON.stringify(error));
+        });
+    },
+    getBoard() {
+      /* http://localhost:8080/api/v1/board/list/${this.id} */
+      /* https://42b1923e-9ac4-4979-b904-912c15c18ea6.mock.pstmn.io/localhost:8080/board/list/id */
+      axios
+        .get(
+          `https://42b1923e-9ac4-4979-b904-912c15c18ea6.mock.pstmn.io/localhost:8080/board/list/id`,
+          {
+            headers: {
+              "X-AUTH-TOKEN": `${localStorage.getItem("access_token")}`,
+            },
+          }
+        )
+        .then((response) => {
+          //서버 사용 시 response.data.nickname
+          this.boardNickname = response.data.nickname;
+          if (this.boardNickname == this.$store.state.userInfo.nickname) {
+            this.checkBoardUser = 1;
+          } else {
+            this.checkBoardUser = 0;
+          }
+          this.board.title = response.data.title;
+          this.board.nickname = response.data.nickname;
+          this.board.content = response.data.content;
+          this.board.createdDate = response.data.createdDate;
+          this.board.modifiedDate = response.data.modifiedDate;
+          response.data.commentResponseDtoList.forEach((a) => {
+            this.board.commentResponseDtoList.push(a);
+          });
+          this.board.content.replace("\n", "<br />");
+          if (this.board.modifiedDate == "") {
+            this.viewDate = this.board.createdDate;
+          } else {
+            this.viewDate = this.board.modifiedDate;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    goEdit() {
+      this.$router.push({
+        name: "ReviewBoardWrite",
+        params: { id: this.id, board: this.board },
+      });
+    },
+    deleteBoard() {
+      if (confirm("정말 삭제하시겠어요 ?")) {
+        axios({
+          method: "delete", // [요청 타입]
+          url: `http://localhost:8080/api/v1/board/list/delete/${this.id}`, // [요청 주소]
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            "X-AUTH-TOKEN": localStorage.getItem("access_token"),
+          }, // [요청 헤더]
+          timeout: 5000, // [타임 아웃 시간]
+        })
+          .then(function (response) {
+            console.log("RESPONSE : " + JSON.stringify(response.data));
+            setTimeout(() => {
+              this.$router.push({ path: "/reviewBoard" });
+            }, 100);
+          })
+          .catch(function (error) {
+            console.log("ERROR : " + JSON.stringify(error));
+          });
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.balloon {
+  position: relative;
+  width: 120px;
+  height: 90px;
+  background: #1b5691;
+  border-radius: 10px;
+  margin: 20px 0px 50px 35px;
+  text-align: center;
+  color: white;
+  line-height: 210%;
+}
+#comment_num {
+  font-size: 35px;
+  text-shadow: 0 0 5px rgba(0, 0, 0, 0.6);
+  padding: 12px 0 6px;
+}
+#comment_title {
+  font-size: 13px;
+  padding: 2px 0 12px 0;
+  line-height: 100%;
+  text-shadow: 0 0 5px rgba(0, 0, 0, 0.6);
+}
+.balloon::after {
+  border-left: 20px solid transparent;
+  border-right: 20px solid transparent;
+  border-top: 20px solid #1b5691;
+  content: "";
+  position: absolute;
+  right: 40px;
+  top: 90px;
+}
+.view_header1 {
+  background-color: #f8f8f8;
+  padding: 15px 30px;
+  font-weight: 700;
+  font-size: 15px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+.comment_header {
+  background-color: #f8f8f8;
+  padding: 15px 30px;
+  font-weight: 700;
+  font-size: 15px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+.manageBtn {
+  margin-left: auto;
+  padding-right: 15px;
+}
+.manageComment {
+  margin-left: 5px;
+}
+.editBtn {
+  color: white;
+  background-color: rgb(0, 195, 255);
+  padding: 0 11px 2px;
+  line-height: 220%;
+  border-radius: 4px;
+  text-shadow: 0 0 5px rgba(0, 0, 0, 0.6);
+}
+.deleteBtn {
+  color: white;
+  background-color: rgb(255, 75, 0);
+  border-color: red;
+  padding: 0 11px 2px;
+  line-height: 220%;
+  border-radius: 4px;
+  text-shadow: 0 0 5px rgba(0, 0, 0, 0.6);
+}
+.view_header2 {
+  border: solid #f8f8f8 3px;
+  padding: 15px 30px;
+  font-weight: 700;
+  font-size: 13px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+.viewContent {
+  white-space: pre-line;
+  padding: 15px 30px;
+}
+.commentContent {
+  white-space: pre-line;
+  padding: 15px 30px;
+  margin-bottom: 10px;
+}
+.writeComment {
+  color: white;
+  background-color: #1bc6ec;
+  border-color: #1bc6ec;
+  padding: 0 11px 2px;
+  line-height: 220%;
+  border-radius: 4px;
+  margin-left: 35px;
+  text-shadow: 0 0 5px rgba(0, 0, 0, 0.6);
+}
+#viewDate {
+  margin-left: auto;
+  float: right;
+  padding-right: 15px;
+  color: #bbb;
+}
+#viewUser svg {
+  padding-right: 5px;
+}
+#viewUser {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+</style>
--- vue-front/src/components/ReviewBoardWrite.vue
+++ vue-front/src/components/ReviewBoardWrite.vue
@@ -31,6 +31,7 @@ import axios from "axios";
 export default {
   data() {
     return {
+      id: "",
       board: {
         title: "",
         nickname: "",
@@ -41,44 +42,72 @@ export default {
   mounted() {
     if (this.$route.params.board != undefined) {
       this.board.title = this.$route.params.board.title;
+      this.board.nickname = this.$route.params.board.nickname;
       this.board.content = this.$route.params.board.content;
+      this.id = this.$route.params.id;
     }
   },
   methods: {
     writeBoard() {
-      this.board.nickname = this.$store.state.userInfo.nickname;
-      //https://reqres.in/api/users
-      //http://localhost:8080/api/v1/board/post
-      axios({
-        method: "post", // [요청 타입]
-        url: "https://reqres.in/api/users", // [요청 주소]
-        data: JSON.stringify(this.board), // [요청 데이터]
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "X-AUTH-TOKEN": localStorage.getItem("access_token"),
-        }, // [요청 헤더]
-        timeout: 5000, // [타임 아웃 시간]
+      if (this.id == "") {
+        this.board.nickname = this.$store.state.userInfo.nickname;
+        //https://reqres.in/api/users
+        //http://localhost:8080/api/v1/board/post
+        axios({
+          method: "post", // [요청 타입]
+          url: "https://reqres.in/api/users", // [요청 주소]
+          data: JSON.stringify(this.board), // [요청 데이터]
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            "X-AUTH-TOKEN": localStorage.getItem("access_token"),
+          }, // [요청 헤더]
+          timeout: 5000, // [타임 아웃 시간]
 
-        //responseType: "json" // [응답 데이터 : stream , json]
-      })
-        .then((response) => {
-          console.log("RESPONSE : " + JSON.stringify(response.data));
-          setTimeout(() => {
-            this.$router.push({ path: "/reviewBoard" });
-          }, 100);
+          //responseType: "json" // [응답 데이터 : stream , json]
         })
-        .catch((error) => {
-          //비로그인 시 로그인 창으로 이동
-          if (error.stateCode == "403") {
-            alert("로그인이 필요한 서비스입니다.");
-            this.$router.push({
-              name: "Login",
-              params: { board: this.board },
-            });
-            return;
-          }
-          console.log("ERROR : " + JSON.stringify(error));
-        });
+          .then((response) => {
+            console.log(response.data);
+            setTimeout(() => {
+              this.$router.push({ path: "/reviewBoard" });
+            }, 100);
+          })
+          .catch((error) => {
+            //비로그인 시 로그인 창으로 이동
+            if (error.stateCode == "403") {
+              alert("로그인이 필요한 서비스입니다.");
+              this.$router.push({
+                name: "Login",
+                params: { board: this.board },
+              });
+              return;
+            }
+            console.log("ERROR : " + JSON.stringify(error));
+          });
+      } else {
+        // http://localhost:8080/api/v1/board/list/update/${this.id}
+        // https://reqres.in/api/users/2
+        axios({
+          method: "put", // [요청 타입]
+          url: `http://localhost:8080/api/v1/board/list/update/${this.id}`, // [요청 주소]
+          data: JSON.stringify(this.board), // [요청 데이터]
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            "X-AUTH-TOKEN": localStorage.getItem("access_token"),
+          }, // [요청 헤더]
+          timeout: 5000, // [타임 아웃 시간]
+
+          //responseType: "json" // [응답 데이터 : stream , json]
+        })
+          .then(function (response) {
+            console.log(response.data);
+          })
+          .catch(function (error) {
+            console.log("ERROR : " + JSON.stringify(error));
+          });
+        setTimeout(() => {
+          this.$router.push({ path: "/reviewBoard" });
+        }, 100);
+      }
     },
   },
 };
